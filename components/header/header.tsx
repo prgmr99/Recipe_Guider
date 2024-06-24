acbf56902a81d7a4b8a72d8932e3e1fc388a92d5
prgmr99 <johnyeom24@naver.com>
2024-06-24 15:52:50 +0900

    feat: 헤더 백그라운드 구현

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,24 +1,31 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import styles from "./header.module.css";
+import Image from "next/image";
+import HeaderBackground from "./header-background";
 
 export default function MainHeader() {
   return (
-    <header className={styles.header}>
-      <Link className={styles.logo} href="/">
-        <img src={logoImg.src} alt="A plate with food on it" />
-        NextLevel Piece
-      </Link>
-      <nav className={styles.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <HeaderBackground />
+      <header className={styles.header}>
+        <Link className={styles.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Piece
+        </Link>
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
+
+// Image 태그를 사용할 때는 .src를 제거하여 사용한다.
