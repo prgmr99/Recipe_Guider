3413efdd20148c01f318e85477e120bb8e07d8f1
prgmr99 <johnyeom24@naver.com>
2024-06-28 15:42:10 +0900

    feat: 데이터 페칭 로딩 컴포넌트 적용

--- app/meals/page.tsx
+++ app/meals/page.tsx
@@ -1,12 +1,18 @@
+import { Suspense } from "react";
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getAllMeals } from "@/lib/meals";
 
-export default async function MealsPage() {
+async function Meals() {
+  const meals: any = await getAllMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
   // next.js에서는 이미 해당 컴포넌트가 기본적으로 서버에서 실행되기 때문에
   // fetch를 사용하지 않아도 되고, 서버에서 직접 가져오면 된다.
-  const meals: any = await getAllMeals();
 
   return (
     <>
@@ -23,8 +29,16 @@ export default async function MealsPage() {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
 }
+
+// next.js에서는 꽤 공격적인 캐싱을 한다.
+// 정확히는 페이지의 데이터를 포함해 방문한 모든 페이지를 캐시하고 다른 페이지로 갔다가 돌아오면
+// 캐시에서 기존 페이지를 로드해 최대한 빨리 보여준다.
