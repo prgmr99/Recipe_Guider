a90208c9b76f7b46c8a38eb3d7320fad0b330642
prgmr99 <johnyeom24@naver.com>
2024-06-30 13:55:50 +0900

    feat: 이미지 파일 읽기 및 미리보기

---
+++ components/meals/image-picker.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { ChangeEvent, useRef, useState } from "react";
+import classes from "./image-picker.module.css";
+import Image from "next/image";
+
+interface ImagePickerProps {
+  label: string;
+  name: string;
+}
+
+export default function ImagePicker({ label, name }: ImagePickerProps) {
+  const [pickedImage, setPickedImage] = useState(null);
+  const imageInput = useRef<HTMLInputElement>(null);
+
+  function handlePickImage() {
+    imageInput.current?.click();
+  }
+
+  function handleImageChange(event: ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files[0];
+
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
+  return (
+    <div className={classes.picker}>
+      <label htmlFor={name}>{label}</label>
+      <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No image picked yet.</p>}
+          {pickedImage && <Image src={pickedImage} alt="Picked Image" fill />}
+        </div>
+        <input
+          className={classes.input}
+          type="file"
+          id={name}
+          accept="image/png, image/jpeg"
+          name={name}
+          ref={imageInput}
+          onChange={handleImageChange}
+          required
+        />
+        <button
+          className={classes.button}
+          type="button"
+          onClick={handlePickImage}
+        >
+          Pick an Image
+        </button>
+      </div>
+    </div>
+  );
+}
